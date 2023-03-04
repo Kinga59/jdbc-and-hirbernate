08fddb2785cebeef757a4aba85ee8543824145ce
Kinga59 <krozmuszynska@gmail.com>
2023-03-04 13:50:03 +0100

    [change] - small update2

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -10,5 +10,16 @@ public class LambdaExercises {
         ChłopakiNiePłaczą boy2 = new ChłopakiNiePłaczą();
         boy2.sing();
 
+        BoysDontCry anonymusClassReference = new BoysDontCry() {
+            @Override
+            public void silnoreki() {
+                System.out.println("Anonimowy Silnoręki");
+            }
+        };
+
+        anonymusClassReference.silnoreki();
+
+        //BoysDontCry = new String(" ");
+
     }
 }
