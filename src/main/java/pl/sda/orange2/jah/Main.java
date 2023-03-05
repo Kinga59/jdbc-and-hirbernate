96ca79ebd8f8974749851b3f8c3908bdd15c133d
Kinga59 <krozmuszynska@gmail.com>
2023-03-05 10:54:33 +0100

    [change] - small update8

--- src/main/java/pl/sda/orange2/jah/Main.java
+++ src/main/java/pl/sda/orange2/jah/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Works...");
-        System.out.println("JDBC and Hibernate - love");
+        System.out.println("!!!!JDBC and Hibernate - love");
     }
 }
