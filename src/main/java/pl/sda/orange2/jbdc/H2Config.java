5ae280cfc6bc1baadd29bc2fe46adf631c7b1fb2
Kinga59 <krozmuszynska@gmail.com>
2023-03-05 14:49:58 +0100

    [change] - maven cd

---
+++ src/main/java/pl/sda/orange2/jbdc/H2Config.java
@@ -0,0 +1,8 @@
+package pl.sda.orange2.jbdc;
+
+public class H2Config {
+    private final static String DB_URL = "jdbc:h2:~/databases/jdbc";
+    private final static String USER = "sa";
+    private final static String PASSWORD = "sa";
+}
+
